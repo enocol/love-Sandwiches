5accae5dcd610e54b88a887f7cd9b46e46302fbb
enocol <eno_col@yahoo.com>
2024-02-20 10:54:11 +0000

    Add function to get stock data and another to calculate suplus and update worksheet

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import pprint
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -14,7 +13,7 @@ CREDENTIALS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDENTIALS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-sales = SHEET.worksheet('sales')
+# sales = SHEET.worksheet('sales')
 
 def get_sales_data():
     """
@@ -65,9 +64,47 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-update_sales_worksheet(data)
 
+def calculate_surplus_data(stock_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock_row = SHEET.worksheet('stock').get_all_values()
+    stock = stock_row[-1]
+    sales_row = SHEET.worksheet('sales').get_all_values()
+    sales = sales_row[-1]
+    surplus_data = []
+    for stock, sales in zip(stock, sales):
+        surplus = int(stock) - int(sales)
+        surplus_data.append(surplus)
+    print(f"Surplus data calculated successfully: {surplus_data}\n")
+    return surplus_data
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+  
+
+def main():
+    """
+    Run all program functions
+    """
+    print("Welcome to Love Sandwiches Data Automation \n")
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(surplus_data)
+
+main()
 
 
